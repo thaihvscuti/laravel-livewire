19270c852a1689a6c6334cf615823cccf5c16006
ThaiHV <thaihv@scuti.asia>
2023-10-06 16:42:21 +0700

    update for sidebar, header, footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,3 @@
-<footer class="footer">
-    <div><a href="https://coreui.io">CoreUI </a><a href="https://coreui.io">Bootstrap Admin Template</a> © 2023 creativeLabs.</div>
-    <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/docs/">CoreUI UI Components</a></div>
+<footer class="footer justify-content-end">
+    <div><a href="https://coreui.io">CoreUI </a><a href="https://coreui.io">Scuti</a> © {{ now()->format('Y') }} .</div>
 </footer>
