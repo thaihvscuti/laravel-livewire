583aed5f3d79f09c145714ac8fb12e656bb1c560
ThaiHV <thaihv@scuti.asia>
2023-10-09 22:34:08 +0700

    update for layout

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,6 +1,15 @@
 <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
     <div class="sidebar-brand d-none d-md-flex">
-        <h4>SCUTI</h4>
+        <a href="{{ route('home') }}" class="text-white" wire:navigate>
+            <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
+                <use xlink:href="assets/brand/coreui.svg#full"></use>
+            </svg>
+        </a>
+        <a href="{{ route('home') }}" class="text-white">
+            <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
+                <use xlink:href="assets/brand/coreui.svg#signet"></use>
+            </svg>
+        </a>
     </div>
     <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
         <li class="nav-item">
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Livewire\Contract\Contract;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Contract\Create;
 use App\Livewire\Contract\Update;
+use App\Livewire\Home;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,11 @@ use App\Livewire\Contract\Update;
 |
 */
 Auth::routes();
-
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('contract', Contract::class)->name('contract.index');
-Route::get('/contract/create', Create::class)->name('contract.create');
-Route::get('/contract/edit/{id}', Update::class)->name('contract.edit');
+Route::group([
+    'middleware' => ['auth']
+], function () {
+    Route::get('/', Home::class)->name('home');
+    Route::get('contract', Contract::class)->name('contract.index');
+    Route::get('/contract/create', Create::class)->name('contract.create');
+    Route::get('/contract/edit/{id}', Update::class)->name('contract.edit');
+});
